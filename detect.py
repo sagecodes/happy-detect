ad3c7e460c5a00daa547db39a089ed5ac3ae3f7c
Sage Elliott <hello@sageelliott.com>
2017-12-29 20:18:26 -0800

    Feat: Add face detection in detect function | Fix: smoile cascade variable name

--- detect.py
+++ detect.py
@@ -2,12 +2,19 @@ import cv2
 
 # load Cascade(s) for face and feature detection
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-face_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
+# Detect face and draw rectangle around region of interest in original image
 def detect(gray, frame):
-    return
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) 
+        roi_gray = gray[y:y+h, x:x+w] 
+        roi_color = frame[y:y+h, x:x+w]
+    return frame
 
 # open webcam for video capture | try changing 0 to 1 for external webcam
+# convert to gray for cascades
 video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
