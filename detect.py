3723df950b5469dc318a6ec6ab5ef6ec301f71d5
Sage Elliott <hello@sageelliott.com>
2017-12-29 20:09:34 -0800

    output detect method in video diplay

--- detect.py
+++ detect.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 face_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
-def detect():
+def detect(gray, frame):
     return
 
 # open webcam for video capture | try changing 0 to 1 for external webcam
@@ -12,6 +12,7 @@ video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
     cv2.imshow('video', frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
